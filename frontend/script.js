47d6dbc23f855198a5f1d5a55115c393ce167baa
stella <mikkamakka89@gmail.com>
2022-05-11 11:26:11 +0200

    day-3

--- frontend/script.js
+++ frontend/script.js
@@ -2,8 +2,50 @@ const menuButtonComponent = function(){
     return `<button id = "menu-btn"></button>`
 }
 
+const beerSectionComponent = function (title, sub, text, id){
+  //return "blablabla"
+  return `
+    <section id="${id}">
+    <h1 class= "beerName">${title}</h1>
+    <h2 class= "beerCompany">${sub}</h2>
+    <h3 class= "beertype">${text}</h3>
+    </section>`
+}
+
+const beerAnchorComponent = function (title, id){
+  return `
+  <a href="#${id}">${title}</a>
+  `
+}
+
+const beerNavComponent = function(inner){
+  return `
+    <nav>${inner}</nav>
+  `
+}
+
+const menuButtonClickEvent = function (event){
+      
+  event.currentTarget.closest("#root").classList.toggle("menu-opened") ;
+}
+
 const loadEvent = function () {
-    const rootElement = document.getElementById("root");
+
+    console.log(beerSectionComponent); // 
+
+    console.log(typeof beerSectionComponent); //function
+
+    console.log(typeof beerSectionComponent()); //string
+
+   // function(){
+   //   console.log("blablablablabla"); //Uncaught SyntaxError: Function statements require a function name 
+  //  }
+
+  /*(function(){
+    console.log("blablablablabla"); // így kiírja 
+  })()*/
+
+    const rootElement = document.getElementById("root"); 
 
     console.log(rootElement.parentElement);
 
@@ -11,18 +53,32 @@ const loadEvent = function () {
     
     const menuButtonElement = document.getElementById("menu-btn");
 
-    menuButtonElement.addEventListener("click", function (event){
-  //  event.currentTarget.classList.toggle("clicked"); //[metódusoknak van zárójele, a toggle() az] kiírja, hogy clicked az events fülön
+    menuButtonElement.addEventListener("click", menuButtonClickEvent);
 
-    //console.log(rootElement);
-   // console.log(event.currentTarget.parentElement); //ezt ne nagyon használjuk, mert ha header-ben van, akkor már nem jó
-    //console.log(event.currentTarget.closest("#root"));
-    //event.currentTarget.closest("#root").classList.toggle("clicked")
-    console.log(menuButtonElement === event.currentTarget)
-    event.currentTarget.closest("#root").classList.toggle("menu-opened") 
-    });
     
-};
+    
+    let beerSections = "";
+
+    for (const beer of beers.cards) {
+      beerSections += beerSectionComponent(beer.title, beer.sub, beer.text);
+    }
+
+    //console.log(beerSections) //így írja ki
+
+    rootElement.insertAdjacentHTML("beforeend", beerSections);
+
+
+
+    let beerAnchors = "";
+  
+    for (const beer of beers.cards) {
+      beerAnchors += beerAnchorComponent(beer.title);
+    }
+
+   // rootElement.insertAdjacentHTML("beforeend", beerAnchors);
+    rootElement.insertAdjacentHTML("beforeend", beerNavComponent(beerAnchors)); //
+}  
+
 
 
 window.addEventListener("load", loadEvent);
